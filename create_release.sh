53ec2c89cc2f7f64492f0b19bd95622f363ca37b
rockiger <marco@rockiger.com>
2023-01-14 16:50:07 +0100

    Update release_script.

--- create_release.sh
+++ create_release.sh
@@ -1,16 +1,18 @@
+version='x.x.x'
+
 rm -r /tmp/reactpress-svn
-mkdir -p /tmp/reactpress-svn/tags/x.x.x
-cp -r ./ /tmp/reactpress-svn/tags/x.x.x/
-rm /tmp/reactpress-svn/tags/x.x.x/.gitignore
-rm -rf /tmp/reactpress-svn/tags/x.x.x/.git
-rm -r /tmp/reactpress-svn/tags/x.x.x/.vscode
-rm -r /tmp/reactpress-svn/tags/x.x.x/assets
-rm /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/*
-rm /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/.gitignore
-rm -r /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/node_modules
-rm -r /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/src
-rm -r /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/public
-rm -rf /tmp/reactpress-svn/tags/x.x.x/admin/js/reactpress-admin/.git
+mkdir -p /tmp/reactpress-svn/tags/$version
+cp -r ./ /tmp/reactpress-svn/tags/$version/
+rm /tmp/reactpress-svn/tags/$version/.gitignore
+rm -rf /tmp/reactpress-svn/tags/$version/.git
+rm -r /tmp/reactpress-svn/tags/$version/.vscode
+rm -r /tmp/reactpress-svn/tags/$version/assets
+rm /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/*
+rm /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/.gitignore
+rm -r /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/node_modules
+rm -r /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/src
+rm -r /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/public
+rm -rf /tmp/reactpress-svn/tags/$version/admin/js/reactpress-admin/.git
 rm -r /tmp/reactpress-svn/trunk
-cp -r /tmp/reactpress-svn/tags/x.x.x/ /tmp/reactpress-svn/
-mv /tmp/reactpress-svn/x.x.x /tmp/reactpress-svn/trunk
+cp -r /tmp/reactpress-svn/tags/$version/ /tmp/reactpress-svn/
+mv /tmp/reactpress-svn/$version /tmp/reactpress-svn/trunk
