ebd4b78ccae31f5a58301db5cc853baa6f147ada
rockiger <marco@rockiger.com>
2023-03-04 08:47:45 +0100

    Add scripts and css as late as possible. That we hopefully don't need to use !important in our css.

--- includes/Core.php
+++ includes/Core.php
@@ -176,7 +176,7 @@ class Core {
 		$plugin_public = new User($this->get_plugin_name(), $this->get_version());
 
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts', 1000);
 		// Add template 
 		$this->loader->add_filter('template_include', $plugin_public, 'repr_change_page_template', 99);
 		// Add rewrite rules
