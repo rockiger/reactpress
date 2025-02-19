78fd1b807a42cffb51edeb64473c931dc9b3d3bc
bendiy <571574+bendiy@users.noreply.github.com>
2025-02-19 09:15:51 -0700

    Fixes #41 - Trim URL slug's leading and trailing slashes for ReactRouter path so full page reload finds the base slug and does not 404.

--- admin/Controller.php
+++ admin/Controller.php
@@ -288,7 +288,16 @@ class Controller {
               throw new LengthException('Client-side routing is only possible on one single page.');
             }
             foreach ($el['pages'] as $page) {
-              add_rewrite_rule('^' . wp_make_link_relative($page['permalink']) . '/(.*)?', 'index.php?pagename=' . wp_make_link_relative($page['permalink']), 'top');
+              add_rewrite_rule(
+                '^' .
+                // Trim leading and trailing slashes to get `^foo/bar/(.*)?` not `^/foo/bar//(.*)?`
+                trim(wp_make_link_relative($page['permalink']), '/') .
+                '/(.*)?',
+                'index.php?pagename=' .
+                // Trim leading and trailing slashes to get `index.php?pagename=foo/bar` not `index.php?pagename=/foo/bar/`
+                trim(wp_make_link_relative($page['permalink']), '/'),
+                'top'
+              );
               Utils::set_public_url_for_dev_server($appname, $page['permalink']);
             }
             flush_rewrite_rules();
